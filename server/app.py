051fe09361534de1f573b15e572c9190fcbd3c2f
Rohan Mathur <rohanmathur34@gmail.com>
2018-08-11 23:02:13 -0700

    Reorganize code files

--- server/app.py
+++ server/app.py
@@ -1,26 +1,11 @@
 import time
 
-from flask import Flask
+from flask import Flask, make_response, jsonify, abort
 import RPi.GPIO as GPIO
 
-#
-# Variables used for the Flask framework.
-#
+from config import *
 
 app = Flask(__name__)
-
-#
-# Variables used to control the GPIO pins.
-#
-
-GPIO_SIGNAL_PIN = 12
-# left
-#DUTY_CYCLE_ON = 5.5
-#DUTY_CYCLE_OFF = 10
-
-# right
-DUTY_CYCLE_ON = 10
-DUTY_CYCLE_OFF = 5.5
 pwm = None
 
 #
@@ -29,38 +14,55 @@ pwm = None
 
 lightsOn = False
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/status')
+def status():
+    return make_response(jsonify({'status': lightsOn}), 200)
 
 @app.route('/on')
 def turn_on():
+    global pwm
+    global lightsOn
+
+    if lightsOn:
+        abort(400)
+
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(GPIO_SIGNAL_PIN, GPIO.OUT)
-    pwm = GPIO.PWM(GPIO_SIGNAL_PIN, 50)
+
+    pwm = GPIO.PWM(GPIO_SIGNAL_PIN, FREQUENCY)
     pwm.start(DUTY_CYCLE_ON)
     pwm.ChangeDutyCycle(DUTY_CYCLE_ON)
-    time.sleep(0.5)
+    time.sleep(DELAY_BETWEEN_REQUESTS)
     pwm.stop()
+
     GPIO.cleanup()
-    return 'Turned on!'
+
+    lightsOn = True
+
+    return make_response(jsonify({'status': lightsOn}), 200)
 
 @app.route('/off')
 def turn_off():
+    global pwm
+    global lightsOn
+
+    if not lightsOn:
+        abort(400)
+
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(GPIO_SIGNAL_PIN, GPIO.OUT)
-    pwm = GPIO.PWM(GPIO_SIGNAL_PIN, 50)
+
+    pwm = GPIO.PWM(GPIO_SIGNAL_PIN, FREQUENCY)
     pwm.start(DUTY_CYCLE_OFF)
     pwm.ChangeDutyCycle(DUTY_CYCLE_OFF)
-    time.sleep(0.5)
+    time.sleep(DELAY_BETWEEN_REQUESTS)
     pwm.stop()
-    GPIO.cleanup()
-    return 'Turned off!'
 
-@app.route('/cleanup')
-def cleanup():
     GPIO.cleanup()
-    return 'Cleaned up!'
+
+    lightsOn = False
+
+    return make_response(jsonify({'status': lightsOn}), 200)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
---
+++ server/config.py
@@ -0,0 +1,12 @@
+# servo pin number
+GPIO_SIGNAL_PIN = 12
+
+# in Hz
+FREQUENCY = 50
+
+# in percentage
+DUTY_CYCLE_ON = 10
+DUTY_CYCLE_OFF = 5.5
+
+# in seconds
+DELAY_BETWEEN_REQUESTS = 0.5
